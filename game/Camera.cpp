2d9523bab275bf81c60bd3d8892676344d9185e5
babulin <787193801@qq.com>
2020-01-26 13:56:25 +0800

    摄影机

--- game/Camera.cpp
+++ game/Camera.cpp
@@ -2,13 +2,13 @@
 
 Camera::Camera(LPDIRECT3DDEVICE9 d3dDevice)
 {
-	OutputDebugString(L"Camera()构造\n");
+	OutputDebugString(TEXT("Camera()构造\n"));
 	p_d3dDevice = d3dDevice;
 }
 
 Camera::~Camera()
 {
-	OutputDebugString(L"~Camera()析构\n");
+	OutputDebugString(TEXT("~Camera()析构\n"));
 
 }
 
@@ -22,9 +22,9 @@ void Camera::SetMatrices()
 	}
 	i+=0.05f;
 	FLOAT fAngle = i;// *(2.0f * D3DX_PI) / 1000.0f;
-	wchar_t buf[50] = { 0 };
-	swprintf_s(buf, 50, L"角度:%0.2f \n", fAngle);
-	OutputDebugString(buf);
+	//wchar_t buf[50] = { 0 };
+	//swprintf_s(buf, 50, TEXT("角度:%0.2f \n"), fAngle);
+	//OutputDebugString(buf);
 	D3DXMatrixRotationY(&Ry, fAngle);
 	D3DXMatrixRotationX(&Rx, fAngle);
 	matWorld = Rx * Ry;
--- game/Exception.h
+++ game/Exception.h
@@ -14,7 +14,7 @@ namespace AHLIN {
 			Exception(const wchar_t* file, unsigned int line, const wchar_t* msg)
 				:file(file), msg(msg), line(line)
 			{
-				wsprintf(buf, L"%s \nLine [%d] %s ", msg,line, file);
+				wsprintf(buf, TEXT("%s \nLine [%d] %s "), msg,line, file);
 			}
 
 			~Exception() {
